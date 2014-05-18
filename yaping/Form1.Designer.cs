e0990cb1dd41b5d13e34b1218c434d4c5fe02449
Jens Heilig <jens@familie-heilig.net>
2014-05-18 13:23:20 +0200

    Yet Another Ping utility
    pings a host with settable interval time and settable payload size.
    
    Special bout this version: The payload returned by the pinged client is verified against the payload sent.

---
+++ yaping/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace yaping
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Erforderliche Designervariable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Verwendete Ressourcen bereinigen.
+    /// </summary>
+    /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Vom Windows Form-Designer generierter Code
+
+    /// <summary>
+    /// Erforderliche Methode für die Designerunterstützung.
+    /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.label1 = new System.Windows.Forms.Label();
+      this.tb_IPaddr = new System.Windows.Forms.TextBox();
+      this.btn_Start = new System.Windows.Forms.Button();
+      this.tb_Result = new System.Windows.Forms.TextBox();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.tb_szPayload = new System.Windows.Forms.TextBox();
+      this.label4 = new System.Windows.Forms.Label();
+      this.tb_Interval = new System.Windows.Forms.MaskedTextBox();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(13, 53);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(60, 13);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "IP-address:";
+      // 
+      // tb_IPaddr
+      // 
+      this.tb_IPaddr.Location = new System.Drawing.Point(88, 50);
+      this.tb_IPaddr.Name = "tb_IPaddr";
+      this.tb_IPaddr.Size = new System.Drawing.Size(141, 20);
+      this.tb_IPaddr.TabIndex = 1;
+      // 
+      // btn_Start
+      // 
+      this.btn_Start.DialogResult = System.Windows.Forms.DialogResult.OK;
+      this.btn_Start.Location = new System.Drawing.Point(418, 263);
+      this.btn_Start.Name = "btn_Start";
+      this.btn_Start.Size = new System.Drawing.Size(75, 23);
+      this.btn_Start.TabIndex = 4;
+      this.btn_Start.Text = "Start Ping";
+      this.btn_Start.UseVisualStyleBackColor = true;
+      this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+      // 
+      // tb_Result
+      // 
+      this.tb_Result.BackColor = System.Drawing.SystemColors.Window;
+      this.tb_Result.Enabled = false;
+      this.tb_Result.Location = new System.Drawing.Point(16, 140);
+      this.tb_Result.Multiline = true;
+      this.tb_Result.Name = "tb_Result";
+      this.tb_Result.ReadOnly = true;
+      this.tb_Result.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+      this.tb_Result.ShortcutsEnabled = false;
+      this.tb_Result.Size = new System.Drawing.Size(396, 146);
+      this.tb_Result.TabIndex = 5;
+      this.tb_Result.TabStop = false;
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label2.Location = new System.Drawing.Point(12, 9);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(168, 24);
+      this.label2.TabIndex = 4;
+      this.label2.Text = "Yet Another Ping";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(13, 79);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(69, 13);
+      this.label3.TabIndex = 0;
+      this.label3.Text = "Payload size:";
+      // 
+      // tb_szPayload
+      // 
+      this.tb_szPayload.Location = new System.Drawing.Point(88, 76);
+      this.tb_szPayload.Name = "tb_szPayload";
+      this.tb_szPayload.Size = new System.Drawing.Size(141, 20);
+      this.tb_szPayload.TabIndex = 2;
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Location = new System.Drawing.Point(13, 105);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(67, 13);
+      this.label4.TabIndex = 0;
+      this.label4.Text = "Interval (ms):";
+      // 
+      // tb_Interval
+      // 
+      this.tb_Interval.HidePromptOnLeave = true;
+      this.tb_Interval.Location = new System.Drawing.Point(88, 102);
+      this.tb_Interval.Mask = "999990";
+      this.tb_Interval.Name = "tb_Interval";
+      this.tb_Interval.PromptChar = ' ';
+      this.tb_Interval.RejectInputOnFirstFailure = true;
+      this.tb_Interval.Size = new System.Drawing.Size(141, 20);
+      this.tb_Interval.TabIndex = 3;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(505, 298);
+      this.Controls.Add(this.tb_Interval);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.tb_Result);
+      this.Controls.Add(this.btn_Start);
+      this.Controls.Add(this.label4);
+      this.Controls.Add(this.tb_szPayload);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.tb_IPaddr);
+      this.Controls.Add(this.label1);
+      this.Name = "Form1";
+      this.Text = "Yet another Ping";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TextBox tb_IPaddr;
+    private System.Windows.Forms.Button btn_Start;
+    private System.Windows.Forms.TextBox tb_Result;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.TextBox tb_szPayload;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.MaskedTextBox tb_Interval;
+  }
+}
+
---
+++ yaping/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+namespace yaping
+{
+  public partial class Form1 : Form
+  {
+    private AbortableBackgroundWorker bw;
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void btn_Start_Click(object sender, EventArgs e)
+    {
+      btn_Start.Text = "Stop Ping";
+      btn_Start.Click -= btn_Start_Click;
+      btn_Start.Click += btn_Stop_Click;
+      tb_Result.Clear();
+
+      bw = new AbortableBackgroundWorker();
+
+      // this allows our worker to report progress during work
+      bw.WorkerReportsProgress = true;
+
+      // what to do in the background thread
+      bw.DoWork += new DoWorkEventHandler(
+      delegate(object o, DoWorkEventArgs args)
+      {
+        BackgroundWorker b = o as BackgroundWorker;
+
+        // run ping() in an endless loop
+        while (true)
+        {
+          b.ReportProgress(10, ping());
+          Thread.Sleep(Convert.ToInt32(tb_Interval.Text));
+        }
+
+      });
+
+      // what to do when progress changed (update the progress bar for example)
+      bw.ProgressChanged += new ProgressChangedEventHandler(
+      delegate(object o, ProgressChangedEventArgs args)
+      {
+        tb_Result.AppendText(string.Format("{0}", args.UserState));// = string.Format("{0}% Completed", args.ProgressPercentage);
+      });
+
+      bw.RunWorkerAsync();
+    }
+
+    private void btn_Stop_Click(object sender, EventArgs e)
+    {
+      btn_Start.Text = "Start Ping";
+      btn_Start.Click -= btn_Stop_Click;
+      btn_Start.Click += btn_Start_Click;
+
+      bw.Abort();
+      bw.Dispose();
+
+    }
+
+    static bool ByteArrayCompare(byte[] a1, byte[] a2)
+    {
+      if (a1.Length != a2.Length)
+        return false;
+
+      for (int i = 0; i < a1.Length; i++)
+        if (a1[i] != a2[i])
+          return false;
+
+      return true;
+    }
+
+    private string ping()
+    {
+      Ping pingSender = new Ping();
+      PingOptions options = new PingOptions();
+
+      string str_result;
+
+      // Use the default Ttl value which is 128,
+      // but change the fragmentation behavior.
+      options.DontFragment = true;
+
+      // Create a buffer of 32 bytes of data to be transmitted.
+      byte[] buffer = Encoding.ASCII.GetBytes(RandomString(Convert.ToInt32(tb_szPayload.Text), false));
+      int timeout = 120;
+      PingReply reply = pingSender.Send(tb_IPaddr.Text, timeout, buffer, options);
+      if (reply.Status == IPStatus.Success)
+      {
+        str_result = String.Format("{0}: ", reply.Address.ToString());
+        str_result += String.Format("RoundTrip time: {0}, ", reply.RoundtripTime);
+        //        tb_Result.AppendText(String.Format("Time to live: {0}, ", reply.Options.Ttl));
+        //        tb_Result.AppendText(String.Format("Don't fragment: {0}\r\n", reply.Options.DontFragment));
+        str_result += String.Format("Buffer size: {0}, ", reply.Buffer.Length);
+        if (ByteArrayCompare(reply.Buffer, buffer))
+        {
+          str_result += "Payload OK!\r\n";
+        }
+        else
+        {
+          str_result += "Payload NOT OK!\r\n";
+        }
+      }
+      else
+      {
+        str_result = "Ping failed\r\n";
+      }
+      return str_result;
+    }
+
+    public string RandomString(int size, bool lowerCase)
+    {
+      StringBuilder builder = new StringBuilder();
+      Random random = new Random();
+      char ch;
+      for (int i = 0; i < size; i++)
+      {
+        ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+        builder.Append(ch);
+      }
+      if (lowerCase)
+        return builder.ToString().ToLower();
+      return builder.ToString();
+    }
+  }
+
+
+
+
+
+  public class AbortableBackgroundWorker : BackgroundWorker
+  {
+
+    private Thread workerThread;
+
+    protected override void OnDoWork(DoWorkEventArgs e)
+    {
+      workerThread = Thread.CurrentThread;
+      try
+      {
+        base.OnDoWork(e);
+      }
+      catch (ThreadAbortException)
+      {
+        e.Cancel = true; //We must set Cancel property to true!
+        Thread.ResetAbort(); //Prevents ThreadAbortException propagation
+      }
+    }
+
+
+    public void Abort()
+    {
+      if (workerThread != null)
+      {
+        workerThread.Abort();
+        workerThread = null;
+      }
+    }
+  }
+}
